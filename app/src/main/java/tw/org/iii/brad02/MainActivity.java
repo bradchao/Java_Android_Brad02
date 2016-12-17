9495adb2bd0dcd9344a6223d9a1219b26c7c6b9d
brad <brad@brad.tw>
2016-12-17 14:35:18 +0800

    v2

--- app/src/main/java/tw/org/iii/brad02/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad02/MainActivity.java
@@ -3,14 +3,18 @@ package tw.org.iii.brad02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-    public MainActivity(){
-        Log.d("brad", "MainActivity()");
-    }
+    private WebView webview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview = (WebView)findViewById(R.id.webview);
+
     }
 }
