2306e5f5b6e01114b73ed50808f7292bb9dd667e
brad <brad@brad.tw>
2016-12-18 15:18:26 +0800

    v21

--- app/src/main/java/tw/org/iii/brad02/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad02/MainActivity.java
@@ -1,6 +1,8 @@
 package tw.org.iii.brad02;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,7 @@ import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView username;
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handler = new UIHandler();
         sp = getSharedPreferences("appdata", MODE_PRIVATE);
         editor = sp.edit();
 
@@ -67,15 +72,23 @@ public class MainActivity extends AppCompatActivity {
     public class BradJS {
         @JavascriptInterface
         public void getName(String info){
-            Log.d("brad", "getName():" + info);
-            // username.setText(info);
+            Message mesg = new Message();
+            handler.sendMessage(mesg);
 
             editor.putString("username", info);
             editor.commit();
 
-            //Log.d("brad", "==> " +sp.getString("username","ok"));
-
+            //username.setText(info);
         }
     }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            username.setText("III");
+        }
+    }
+
 
 }
